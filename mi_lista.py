bc9239d4b2320ace74653a343bcedede8b26feda
Valentin Loya <a.22308051281244@cbtis128.edu.mx>
2024-09-09 10:12:44 -0600

    Add files via upload

---
+++ mi_lista.py
@@ -0,0 +1,17 @@
+print("Ejemplo de listas")
+arcoiris=["verde","azul","rojo"]
+print(arcoiris)
+longitud=len(arcoiris)
+print("Elementos que contiene la lista arcoiris",longitud)
+print(f"Elementos que contiene la lista arcoiris v2 {longitud}")
+print("accediendo a un elemento de la lista")
+print(arcoiris[1])
+print(f"elemento en la posicion 0 es {arcoiris[0]}")
+print(f"El primer color del arcoiris es:",arcoiris[0])
+print("agregar a un elemento de la lista")
+print(arcoiris)
+arcoiris.append("naranja")
+print(arcoiris)
+print("Listar los elementos de la lista ciclo for")
+for Loya in arcoiris:
+    print(Loya)
