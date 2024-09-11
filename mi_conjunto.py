ae14f60e2d9f4b78ca23355f5c0a5318be418932
Valentin Loya <a.22308051281244@cbtis128.edu.mx>
2024-09-11 07:51:17 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,6 @@
+print("--Ejemplo conjuntos set---")
+tipodeportes=("futbol","tenis","basquetbol")
+print(tipodeportes)
+print("Lista elementos del cnjunto tipodeportes lopp for")
+for loya in tipodeportes:
+    print(loya)
---
+++ mi_diccionario.py
@@ -0,0 +1,12 @@
+
+print("--Ejemplo conjuntos set---")
+tipodeportes=("futbol","tenis","basquetbol")
+print(tipodeportes)
+print("Lista elementos del cnjunto tipodeportes lopp for")
+for loya in tipodeportes:
+    print(loya)thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
